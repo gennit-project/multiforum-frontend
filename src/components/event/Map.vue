449d39330ddf6e52b41aaf1338c93dfbc4c12eaf
Catherine Luse <catherine.luse@gmail.com>
2021-12-03 21:52:19 -0700

    Google Maps markers emit events in setup()

--- src/components/event/Map.vue
+++ src/components/event/Map.vue
@@ -1,27 +1,38 @@
 <script lang="js">
 import { defineComponent, onMounted, ref } from "vue";
-import { Loader } from '@googlemaps/js-api-loader'
+import { Loader } from '@googlemaps/js-api-loader';
+import useEmitter from '@/composables/useEmitter';
+import { useRouter } from 'vue-router'
 import config from "@/config";
 
 export default defineComponent({
-  setup(props) {
+  setup(props, { emit }) {
+    const emitter = useEmitter()
+    const router = useRouter()
     const loader = new Loader({ apiKey: config.googleMapsApiKey })
     const mapDiv = ref(null)
     let map = ref(null)
 
     onMounted(async () => {
       await loader.load()
-      debugger;
       // eslint-disable-next-line
-      map = new google.maps.Map(mapDiv.value, {
+      map.value = new google.maps.Map(mapDiv.value, {
         center: { lat: 33.4255, lng: -111.94 },
         zoom: 7,
         mapTypeId: "terrain"
       })
 
+      emitter.on("highlightEvent", eventId => {
+        emit('highlightEvent', eventId);
+      });
+
+      emitter.on("openPreview", eventId => {
+        emit('openPreview', eventId);
+      });
+
       for (let i = 0; i < props.events.length; i++) {
         const event = props.events[i]
-        
+
         if (event.location) {
           // eslint-disable-next-line
           const marker = new google.maps.Marker({
@@ -29,29 +40,35 @@ export default defineComponent({
               lat: event.location.latitude,
               lng: event.location.longitude
             },
-            map,
+            map: map.value,
             title: event.title,
             clickable: true,
             draggable: true,
-            mouseover: () => {
-              this.$emit('highlightEvent', event.id);
-              this.$router.push(`#${event.id}`)
-            },
-            mouseout: () => {
-              this.$emit('highlightEvent', 0);
-            },
             icon: {
               url: event.id === props.highlightedEventId
               ? require('@/assets/images/highlighted-place-icon.svg').default
               : require('@/assets/images/place-icon.svg').default,
               scaledSize: { width: 20, height: 20 },
             },
-            click: () => {
-              this.center = { lat: event.location.latitude, lng: event.location.longitude };
-              this.$emit('highlightEvent', event.id);
-              this.$emit('openPreview', event);
-            }
           });
+
+          marker.addListener("click", () => {
+            map.value.setZoom(8);
+            map.value.setCenter(marker.getPosition());
+            emit("highlightEvent", event.id)
+            emit("openPreview", event.id)
+          })
+
+          marker.addListener("mouseover", () => {
+            emit('highlightEvent', event.id);
+            router.push({
+              name: `#${event.id}`
+            })
+          })
+
+          marker.addListener("mouseout", () => {
+            emit('highlightEvent', 0);
+          })
 
           // eslint-disable-next-line
           const infowindow = new google.maps.InfoWindow({
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { router } from './router';
 import "./index.css";
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 import { DefaultApolloClient } from '@vue/apollo-composable';
+import mitt from 'mitt';
 import config from './config';
 
 // Cache implementation
@@ -45,6 +46,9 @@ const app = createApp({
 
   render: () => h(App)
 })
+
+const emitter = mitt();
+app.config.globalProperties.emitter = emitter;
 
 app.use(router)
    .mount("#app");
