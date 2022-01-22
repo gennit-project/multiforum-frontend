39d3218a99da4cf2ab83c5161ee727507fc1160a
Catherine Luse <catherine.luse@gmail.com>
2022-01-22 12:07:36 -0700

    Add buttons to text editor

--- src/components/forms/TextEditor.vue
+++ src/components/forms/TextEditor.vue
@@ -4,53 +4,129 @@ import "@github/markdown-toolbar-element";
 
 export default defineComponent({
   setup() {
-    const tabs = [
-      { name: "Write", href: "#", current: true },
-      { name: "Preview", href: "#", current: false },
-    ];
-    const selected = ref('')
-    return { 
-      tabs,
-      selected
+    const showFormatted = ref(false);
+    return {
+      showFormatted,
+      text: "",
     };
   },
   methods: {
     setTab(selected: string) {
       this.selected = selected;
-    }
-  }
+    },
+  },
 });
 </script>
 <template>
-  <div class="m-5">
+  <div class="m-5 comment-caret border rounded p-3 max-w-3xl">
+    <div class="hidden sm:block">
+      <div class="border-b border-gray-200 inline-flex">
+        <nav
+          @click="showFormatted = false"
+          class="flex space-x-8 cursor-pointer"
+          aria-label="Tabs"
+        >
+          <p
+            :class="[
+              showFormatted === false
+                ? 'border-indigo-500 text-indigo-600'
+                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+              'whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm',
+            ]"
+            :aria-current="showFormatted === true ? 'page' : undefined"
+          >
+            Write
+          </p>
+        </nav>
+        <nav
+          @click="showFormatted = true"
+          class="flex space-x-8 cursor-pointer"
+          aria-label="Tabs"
+        >
+          <p
+            :class="[
+              showFormatted === true
+                ? 'border-indigo-500 text-indigo-600'
+                : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+              'whitespace-nowrap py-2 px-1 border-b-2 font-medium text-sm',
+            ]"
+            :aria-current="showFormatted === false ? 'page' : undefined"
+          >
+            Preview
+          </p>
+        </nav>
+      </div>
+      <markdown-toolbar for="textarea_id" class="block space-x-2 float-right">
+        <md-bold><i class="fas fa-bold"></i></md-bold>
+        <md-header><i class="fas fa-heading"></i></md-header>
+        <md-italic><i class="fas fa-italic"></i></md-italic>
+        <md-quote><i class="fas fa-quote-left"></i></md-quote>
+        <md-code><i class="fas fa-code"></i></md-code>
+        <md-link><i class="fas fa-link"></i></md-link>
+        <md-image><i class="fas fa-image"></i></md-image>
+        <md-unordered-list><i class="fas fa-list-ul"></i></md-unordered-list>
+        <md-ordered-list><i class="fas fa-list-ol"></i></md-ordered-list>
+        <md-task-list><i class="far fa-check-square"></i></md-task-list>
+        <!-- <md-mention>@</md-mention>
+      <md-ref>#</md-ref>
+      <button data-md-button>Custom button</button> -->
+      </markdown-toolbar>
+    </div>
 
-  <select v-model="selected">
-    <option disabled value="">Please Select</option>
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-
-    {{ selected }}
-
-    <markdown-toolbar for="textarea_id" class="block space-x-2">
-      <md-bold><span class="font-bold">B</span></md-bold>
-      <md-header>H</md-header>
-      <md-italic><span class="italic">I</span></md-italic>
-      <md-quote>quote</md-quote>
-      <md-code>code</md-code>
-      <md-link>link</md-link>
-      <md-image>image</md-image>
-      <md-unordered-list>unordered-list</md-unordered-list>
-      <md-ordered-list>ordered-list</md-ordered-list>
-      <md-task-list>task-list</md-task-list>
-      <md-mention>mention</md-mention>
-      <md-ref>ref</md-ref>
-      <button data-md-button>Custom button</button>
-    </markdown-toolbar>
-
-    
-    <textarea id="textarea_id" class="w-full h-64"></textarea>
+    <textarea
+      v-show="showFormatted === false"
+      id="textarea_id"
+      class="
+        mt-1
+        shadow-sm
+        block
+        w-full
+        focus:ring-indigo-500 focus:border-indigo-500
+        sm:text-sm
+        border border-gray-300
+        rounded-md
+      "
+      placeholder="preformatted"
+      :value="text"
+    ></textarea>
+    <textarea
+      v-show="showFormatted === true"
+      id="textarea_id"
+      class="
+        mt-1
+        shadow-sm
+        block
+        w-full
+        focus:ring-indigo-500 focus:border-indigo-500
+        sm:text-sm
+        border border-gray-300
+        rounded-md
+      "
+      placeholder="formatted"
+      :value="text"
+    ></textarea>
     <p>Markdown formatting is supported</p>
   </div>
 </template>
+
+<style>
+textarea {
+  min-height: 100px;
+  max-height: 500px;
+}
+
+.comment-caret::before {
+  display: block;
+  position: absolute;
+  top: 11px;
+  right: 100%;
+  left: -8px;
+  display: block;
+  width: 8px;
+  height: 16px;
+  pointer-events: none;
+  content: " ";
+  -webkit-clip-path: polygon(0 50%, 100% 0, 100% 100%);
+  clip-path: polygon(0 50%, 100% 0, 100% 100%);
+}
+</style>
