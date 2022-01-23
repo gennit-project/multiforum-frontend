77034ae3a596465e537b606d3e7b1bb91981aa11
Catherine Luse <catherine.luse@gmail.com>
2022-01-22 19:46:08 -0700

    Text editor renders markdown formatting properly

--- src/components/forms/TextEditor.vue
+++ src/components/forms/TextEditor.vue
@@ -102,28 +102,18 @@ export default defineComponent({
         v-model="text"
       ></textarea>
       <Markdown
-        class="
-          mt-1
-          py-2
-          px-3
-          shadow-sm
-          block
-          w-full
-          focus:ring-indigo-500 focus:border-indigo-500
-          sm:text-sm
-          border border-gray-300
-          rounded-md
-          textbox
-        "
+        class="prose"
         v-show="showFormatted"
         :source="text"
+        linkify
+        html
       />
       <p class="text-xs">Markdown formatting is supported</p>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style>
 .textbox {
   min-height: 100px;
   max-height: 500px;
@@ -139,4 +129,5 @@ export default defineComponent({
   border-top-left-radius: 0.25rem;
   border-top-right-radius: 0.25rem;
 }
+
 </style>
