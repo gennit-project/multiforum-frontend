8db9700c7934d33caea8576ba82d43a789157a02
Catherine Luse <catherine.luse@gmail.com>
2022-08-13 16:32:20 -0700

    Conditionally add default channel ID

--- src/components/event/defaultEventFormValues.js
+++ src/components/event/defaultEventFormValues.js
@@ -10,7 +10,7 @@ export default (channelId) => {
     title: "",
     description: "",
     selectedTags: [],
-    selectedChannels: [channelId],
+    selectedChannels: channelId ? [channelId] : [],
     address: "",
     latitude: 0,
     longitude: 0,
