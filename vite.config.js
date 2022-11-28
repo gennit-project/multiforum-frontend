cffafdd90629fb0cbdd439789a1161fb20240088
Catherine Luse <gennitdev@gmail.com>
2022-11-27 20:29:03 -0700

    Upgrade packages

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { esbuildCommonjs } from '@originjs/vite-plugin-commonjs'
 import { viteCommonjs } from '@originjs/vite-plugin-commonjs'
-// const path = require("path");
-
-
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, 'env');
-
+export default defineConfig(() => {
   return {
     plugins: [vue(), viteCommonjs()],
     optimizeDeps: {
       include: ['luxon']
     },
-    // esbuildOptions: {
-    //   plugins: [esbuildCommonjs('luxon')]
-    // },
     commonjsOptions: {
       include: [/node_modules/]
     },
