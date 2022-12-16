b70d80b1f5dba2d5a764cca1191c83d5bc31c858
Catherine Luse <gennitdev@gmail.com>
2022-12-15 17:02:58 -0700

    Revert hello world changes

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
-<template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
-</template>
-
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import { defineComponent, computed } from "vue";
+import Topnav from "./components/nav/Topnav.vue";
+import SiteSidenav from "./components/nav/SiteSidenav.vue";
+import { useRoute } from "vue-router";
 
 export default defineComponent({
-  name: 'App',
+  setup() {
+    const route = useRoute();
 
+    const channelId = computed(() => {
+      return route.params.channelId;
+    });
+
+    return { channelId }
+  },
+  name: "App",
   components: {
-    HelloWorld,
+    Topnav,
+    SiteSidenav,
   },
-
-  data () {
+  data() {
     return {
-      //
-    }
+      showMobileDropdown: false,
+    };
   },
-})
+  methods: {
+    toggleMobileDropdown() {
+      this.showMobileDropdown = !this.showMobileDropdown;
+    },
+  },
+});
 </script>
+
+<template>
+  <div class="h-screen">
+    <nav class="bg-gray-800">
+      <Topnav @toggleMobileDropdown="toggleMobileDropdown" />
+      <SiteSidenav :show-mobile-dropdown="showMobileDropdown" @click="showMobileDropdown = false"/>
+    </nav>
+    <div v-if="!channelId">
+      <router-view></router-view>
+    </div>
+    
+    <router-view v-else></router-view>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
