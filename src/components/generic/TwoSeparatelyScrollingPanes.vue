e3702ac34d0fe352081f4d06d675532327855cb9
catherineluse <catherineluse@gmail.com>
2023-01-22 21:29:28 -0700

    Resize to one column at medium width

--- src/components/generic/TwoSeparatelyScrollingPanes.vue
+++ src/components/generic/TwoSeparatelyScrollingPanes.vue
@@ -5,10 +5,10 @@ import { useDisplay } from 'vuetify'
 export default defineComponent({
 
     setup() {
-        const { smAndDown } = useDisplay();
+        const { mdAndDown } = useDisplay();
 
         return {
-            smAndDown
+            mdAndDown
         };
     },
     components: {
@@ -19,14 +19,14 @@ export default defineComponent({
 <template>
     <div class="flex flex-row ">
         <div 
-          :class="[!smAndDown ? 'constrain-height' : '']"
+          :class="[!mdAndDown ? 'constrain-height' : '']"
           class="
             lg:overflow-y-auto
             min-w-left
         ">
             <slot name="leftpane"></slot>
         </div>
-        <div v-if="!smAndDown" class="lg:max-h-screen lg:overflow-y-auto min-w-right">
+        <div v-if="!mdAndDown" class="lg:max-h-screen lg:overflow-y-auto min-w-right">
             <slot name="rightpane"></slot>
         </div>
     </div>
