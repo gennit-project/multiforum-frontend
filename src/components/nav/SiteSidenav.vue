7f3acbb0094cade792b10f67292e2d64a2c792fe
Catherine Luse <gennitdev@gmail.com>
2023-04-24 14:57:15 -0700

    Dropdown mobile nav links are correct

--- src/components/nav/SiteSidenav.vue
+++ src/components/nav/SiteSidenav.vue
@@ -1,11 +1,27 @@
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
 import { useAuth0 } from "@auth0/auth0-vue";
+import CalendarIcon from "@/components/icons/CalendarIcon.vue";
+import LocationIcon from "@/components/icons/LocationIcon.vue";
+import DiscussionIcon from "@/components/icons/DiscussionIcon.vue";
+import ChannelIcon from "@/components/icons/ChannelIcon.vue";
 
 export default defineComponent({
+  components: {
+    CalendarIcon,
+    LocationIcon,
+    DiscussionIcon,
+    ChannelIcon,
+  },
   setup() {
     const { isAuthenticated, logout, loginWithRedirect, user } = useAuth0();
 
+    const username = computed(() => {
+      if (user) {
+        return user.value.username;
+      }
+      return "";
+    });
     return {
       isAuthenticated,
       login: () => {
@@ -14,7 +30,7 @@ export default defineComponent({
       logout: () => {
         logout({ returnTo: "http://localhost:5173/logout" });
       },
-      user
+      username,
     };
   },
   props: {
@@ -25,72 +41,43 @@ export default defineComponent({
   },
 });
 </script>
-
 <template>
   <div v-if="showMobileDropdown" class="lg:hidden" id="mobile-menu">
-    <div class="px-2 pt-2 pb-3 space-y-1">
+    <div class="mx-2 px-2 pt-2 pb-3 space-y-1">
       <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
       <router-link
-        to="/"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-        "
+        to="/events/list/search"
+        class="flex items-center hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
         active-class="bg-gray-900 text-white"
       >
-        Events
+        <CalendarIcon class="-ml-0.5 h-4 w-4 mr-1" />
+        Online Events
+      </router-link>
+      <router-link
+        to="/map"
+        class="flex items-center hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
+        active-class="bg-gray-900 text-white"
+        ><LocationIcon class="-ml-0.5 h-4 w-4 mr-1" />
+        In-person Events
       </router-link>
       <router-link
         to="/discussions"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-        "
+        class="flex items-center hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
         active-class="bg-gray-900 text-white"
-      >
+        ><DiscussionIcon class="-ml-0.5 h-4 w-4 mr-1" />
         Discussions
       </router-link>
       <router-link
         to="/channels"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-        "
+        class="flex items-center hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
         active-class="bg-gray-900 text-white"
-      >
+        ><ChannelIcon class="-ml-0.5 h-4 w-4 mr-1" />
         Channels
       </router-link>
       <router-link
         v-if="isAuthenticated"
-        :to="`u/${user.username}`"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-        "
+        :to="`/u/${username}`"
+        class="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
         active-class="bg-gray-900 text-white"
       >
         My Profile
@@ -98,18 +85,7 @@ export default defineComponent({
       <button
         v-if="!isAuthenticated"
         @click="login"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-          w-full
-          text-left
-        "
+        class="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium w-full text-left"
         active-class="bg-gray-900 text-white"
       >
         Log In
@@ -118,16 +94,7 @@ export default defineComponent({
         v-if="isAuthenticated"
         to="/logout"
         @click="logout"
-        class="
-          text-gray-300
-          hover:bg-gray-700 hover:text-white
-          block
-          px-3
-          py-2
-          rounded-full
-          text-base
-          font-medium
-        "
+        class="hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-full text-base font-medium"
         active-class="bg-gray-900 text-white"
       >
         Sign Out
