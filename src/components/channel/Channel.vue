7c924deb1de316fcccf33d3cb37c7c90365a2dc8
Catherine Luse <gennitdev@gmail.com>
2023-11-16 22:06:04 -0700

    Channel icon is smaller at mobile width

--- src/components/channel/Channel.vue
+++ src/components/channel/Channel.vue
@@ -127,7 +127,7 @@ export default defineComponent({
         <Avatar
           class="-mt-24 mb-2 h-24 w-24 border-4 border-white dark:border-gray-800"
           :text="channelId"
-          :is-large="true"
+          :is-medium="true"
         />
       </div>
       <article
