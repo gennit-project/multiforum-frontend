9860fdda65107b53710dbe21f351d3e58e9251ea
Catherine Luse <gennitdev@gmail.com>
2024-02-03 10:34:04 -0700

    Notification is no longer behind channel sidebar

--- src/components/generic/Notification.vue
+++ src/components/generic/Notification.vue
@@ -30,7 +30,7 @@ export default defineComponent({
   <!-- Global notification live region, render this permanently at the end of the document -->
   <div
     aria-live="assertive"
-    class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 mt-12 sm:items-start sm:p-6"
+    class="pointer-events-none fixed inset-0 flex items-end px-4 py-6 mt-12 sm:items-start sm:p-6 z-50"
   >
     <div class="flex w-full flex-col items-center space-y-4 sm:items-end">
       <!-- Notification panel, dynamically insert this into the live region when it needs to be displayed -->
