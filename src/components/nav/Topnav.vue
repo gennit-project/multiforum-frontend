58f20b84a30b55dfa5693fce7087fe1e4d4cb818
Catherine Luse <gennitdev@gmail.com>
2024-02-03 14:22:13 -0700

    Header looks better with light theme

--- src/components/nav/Topnav.vue
+++ src/components/nav/Topnav.vue
@@ -113,7 +113,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="z-10 w-full bg-gray-100 shadow dark:bg-black">
+  <div class="z-10 w-full bg-gray-100 mb-1 dark:bg-black">
     <div
       :class="[smAndDown ? 'px-6 py-2' : 'px-10 py-3']"
       class="flex items-center justify-between"
