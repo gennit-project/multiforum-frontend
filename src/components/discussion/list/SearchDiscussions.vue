b8feb61f4c956f5a829410bd346bee8da440ba4b
Catherine Luse <gennitdev@gmail.com>
2024-02-23 16:45:22 -0700

    Reduce vertical space in forum

--- src/components/discussion/list/SearchDiscussions.vue
+++ src/components/discussion/list/SearchDiscussions.vue
@@ -240,7 +240,7 @@ export default defineComponent({
 <template>
   <v-container
     :class="[channelId ? '' : 'my-6 max-w-4xl']"
-    class="justify-center rounded-lg bg-white dark:bg-gray-800 sm:px-4 sm:py-4 lg:px-10 lg:py-6"
+    class="justify-center rounded-lg bg-white dark:bg-gray-800 sm:px-4 lg:px-10 py-0"
   >
     <SitewideDiscussionList
       v-if="!channelId"
