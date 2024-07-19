38c90e815f6bb6dfef34dd6e728241c075f2ffbc
Catherine Luse <gennitdev@gmail.com>
2024-07-18 22:50:26 -0700

    Fix dark mode

--- src/vuetify.js
+++ src/vuetify.js
@@ -1,5 +1,5 @@
 
-import 'vuetify/styles';
+// import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
