7e8d9642b55746e443aa419dd83711fde3470543
Catherine Luse <gennitdev@gmail.com>
2024-07-30 18:30:37 -0700

    Refetch tag list when search input changes

--- src/graphQLData/tag/queries.ts
+++ src/graphQLData/tag/queries.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client/core";
 
 export const GET_TAGS = gql`
-  query getTags {
-    tags {
+  query getTags($where: TagWhere) {
+    tags(where: $where, options: { limit: 10 }) {
       text
     }
   }
