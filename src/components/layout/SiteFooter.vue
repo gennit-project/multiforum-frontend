f4588114cf3d9a429f73c596be1d544e543bca37
Catherine Luse <gennitdev@gmail.com>
2024-08-01 17:34:14 -0700

    Change order of items in footer

--- src/components/layout/SiteFooter.vue
+++ src/components/layout/SiteFooter.vue
@@ -8,23 +8,7 @@ export default defineComponent({
 <template>
   <div>
     <div
-      class="w-full items-center justify-center text-wrap bg-gray-800 p-4 text-white"
-    >
-      This project is in alpha. I have hosted it publicly so that others can
-      help me test the app. If you have feedback or technical problems, please
-      open an issue in the
-      <a
-        class="ml-1 text-blue-500 underline"
-        target="_blank"
-        href="https://github.com/gennit-project/multiforum-frontend/issues"
-      >GitHub repo</a>, or email me at
-      <a
-        class="ml-1 text-blue-500"
-        href="mailto:gennitdev@gmail.com"
-      >gennitdev@gmail.com</a>.
-    </div>
-    <div
-      class="w-full flex items-center justify-center gap-2 text-wrap bg-black p-4 text-white"
+      class="flex w-full items-center justify-center gap-2 text-wrap bg-black p-4 text-white"
     >
       <router-link
         :to="`/privacy-policy`"
@@ -39,6 +23,22 @@ export default defineComponent({
       >
         Terms of Use
       </router-link>
+    </div>
+    <div
+      class="w-full items-center justify-center text-wrap bg-gray-800 p-4 text-white"
+    >
+      This project is in alpha. I have hosted it publicly so that others can
+      help me test the app. If you have feedback or technical problems, please
+      open an issue in the
+      <a
+        class="ml-1 text-blue-500 underline"
+        target="_blank"
+        href="https://github.com/gennit-project/multiforum-frontend/issues"
+      >GitHub repo</a>, or email me at
+      <a
+        class="ml-1 text-blue-500"
+        href="mailto:gennitdev@gmail.com"
+      >gennitdev@gmail.com</a>.
     </div>
   </div>
 </template>
