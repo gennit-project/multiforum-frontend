da3a4b9e2f980e6a5907a6022d57a9a6acec41ee
Catherine Luse <gennitdev@gmail.com>
2024-08-17 02:06:29 -0700

    Refine the grid of images

--- src/components/discussion/detail/DiscussionAlbum.vue
+++ src/components/discussion/detail/DiscussionAlbum.vue
@@ -10,37 +10,44 @@ export default defineComponent({
       required: true,
     },
   },
-  setup(props) {
+  setup() {
     const getImageClass = (index: number, total: number) => {
-      if (total === 1) return "col-span-3"; // full width
-      if (total === 2) return "col-span-2"; // half width
-
-      switch (total % 3) {
-        case 0:
-          return "col-span-1";
-        case 1:
-          return index === total - 1 ? "col-span-2" : "col-span-1";
-        case 2:
-          return index >= total - 2 ? "col-span-2" : "col-span-1";
+      /*
+        - for a single image, display it full width.
+        - for two images, display each at half width.
+        - for three images, display the first at full width and the second at half width.
+        - for four images, display the first at full width and the rest at one-third width.
+        - for five images, display the first two at half width and the rest at one-third width.
+        - for six images, display the first at full width, next two at half width and the next three at full width.
+     */
+      if (total === 1) {
+        return "col-span-12";
+      } else if (total === 2) {
+        return "col-span-6";
+      } else if (total === 3) {
+        return index === 0 ? "col-span-12" : "col-span-6";
+      } else if (total === 4) {
+        return index === 0 ? "col-span-12" : "col-span-4";
+      } else if (total === 5) {
+        return index < 2 ? "col-span-6" : "col-span-4";
+      } else if (total === 6) {
+        return index === 0
+          ? "col-span-12"
+          : index < 3
+            ? "col-span-6"
+            : "col-span-4";
       }
+      return "col-span-4";
     };
-
-    const gridColumns = computed(() => {
-      if (props.album.Images.length === 1 || props.album.Images.length === 2) {
-        return "grid-cols-2";
-      }
-      return "grid-cols-3";
-    });
 
     return {
       getImageClass,
-      gridColumns,
     };
   },
 });
 </script>
 <template>
-  <div class="grid gap-4" :class="gridColumns">
+  <div class="grid grid-cols-12 gap-1">
     <div
       v-for="(image, index) in album.Images"
       :key="image.id"
