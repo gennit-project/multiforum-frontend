e80ba9c4c94d647178b930cbfe88573b9c9e32b2
Catherine Luse <gennitdev@gmail.com>
2024-08-17 01:58:34 -0700

    Create image grid

--- src/components/discussion/detail/DiscussionAlbum.vue
+++ src/components/discussion/detail/DiscussionAlbum.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, computed } from "vue";
 import { Album } from "@/__generated__/graphql";
 
 export default defineComponent({
@@ -11,21 +11,47 @@ export default defineComponent({
     },
   },
   setup(props) {
-    console.log("album", props.album);
+    const getImageClass = (index: number, total: number) => {
+      if (total === 1) return "col-span-3"; // full width
+      if (total === 2) return "col-span-2"; // half width
 
-    return {};
+      switch (total % 3) {
+        case 0:
+          return "col-span-1";
+        case 1:
+          return index === total - 1 ? "col-span-2" : "col-span-1";
+        case 2:
+          return index >= total - 2 ? "col-span-2" : "col-span-1";
+      }
+    };
+
+    const gridColumns = computed(() => {
+      if (props.album.Images.length === 1 || props.album.Images.length === 2) {
+        return "grid-cols-2";
+      }
+      return "grid-cols-3";
+    });
+
+    return {
+      getImageClass,
+      gridColumns,
+    };
   },
 });
 </script>
 <template>
-  <div class="border-white">
-    <div v-for="image in album.Images" :key="image.id">
+  <div class="grid gap-4" :class="gridColumns">
+    <div
+      v-for="(image, index) in album.Images"
+      :key="image.id"
+      :class="getImageClass(index, album.Images.length)"
+    >
       <img
         :src="image.url"
-        :alt="image.title"
-        class="h-36 w-36 rounded-lg object-cover shadow-sm"
+        :alt="image.alt"
+        class="w-full object-cover shadow-sm"
       />
-      <figcaption>{{ image.title }}</figcaption>
+      <figcaption class="text-center">{{ image.alt }}</figcaption>
     </div>
   </div>
 </template>
