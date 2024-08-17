c54a1fa1cf88e05b2a5013782c791d2bbb64be76
Catherine Luse <gennitdev@gmail.com>
2024-08-17 02:19:57 -0700

    Image expands when clicked

--- src/components/discussion/detail/DiscussionAlbum.vue
+++ src/components/discussion/detail/DiscussionAlbum.vue
@@ -1,29 +1,74 @@
 <script lang="ts">
-import { defineComponent, PropType, computed } from "vue";
+import { defineComponent, ref, PropType } from "vue";
+import VueEasyLightbox from "vue-easy-lightbox";
 import { Album } from "@/__generated__/graphql";
 
 export default defineComponent({
   name: "DiscussionAlbum",
+  components: {
+    VueEasyLightbox,
+  },
   props: {
     album: {
       type: Object as PropType<Album>,
       required: true,
     },
   },
-  setup() {
-    return {};
+  setup(props) {
+    const visibleRef = ref(false);
+    const indexRef = ref(0);
+    const embeddedImages = ref(
+      props.album.Images.map((image) => image.url)
+    );
+
+    const handleImageClick = (index: number) => {
+      indexRef.value = index;
+      visibleRef.value = true;
+    };
+
+    const onHide = () => {
+      visibleRef.value = false;
+    };
+
+    return {
+      visibleRef,
+      indexRef,
+      embeddedImages,
+      handleImageClick,
+      onHide,
+    };
   },
 });
 </script>
+
 <template>
   <div class="grid grid-cols-12 gap-1">
-    <div v-for="image in album.Images" :key="image.id" :class="'col-span-6'">
+    <div
+      v-for="(image, index) in album.Images"
+      :key="image.id"
+      :class="'col-span-6'"
+      @click="handleImageClick(index)"
+    >
       <img
         :src="image.url"
         :alt="image.alt"
-        class="w-full object-cover shadow-sm"
-      />
-      <figcaption class="text-center">{{ image.alt }}</figcaption>
+        class="w-full object-cover shadow-sm cursor-pointer"
+      >
+      <figcaption class="text-center">
+        {{ image.alt }}
+      </figcaption>
     </div>
+    <vue-easy-lightbox
+      :visible="visibleRef"
+      :imgs="embeddedImages"
+      :index="indexRef"
+      @hide="onHide"
+    />
   </div>
 </template>
+
+<style scoped>
+img {
+  cursor: pointer;
+}
+</style>
