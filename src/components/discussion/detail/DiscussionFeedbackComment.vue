67707807a188d160836b6d4325f52e7fa9ed7b4b
Catherine Luse <gennitdev@gmail.com>
2024-08-17 00:43:35 -0700

    Add menu button to discussion feedback comment

--- src/components/discussion/detail/DiscussionFeedbackComment.vue
+++ src/components/discussion/detail/DiscussionFeedbackComment.vue
@@ -49,6 +49,53 @@ export default defineComponent({
       <span class="whitespace-nowrap">{{
         `gave feedback in ${comment.Channel?.uniqueName} ${timeAgo(new Date(comment.createdAt))}`
       }}</span>
+      <MenuButton
+        v-if="commentMenuItems.length > 0"
+        id="commentMenu"
+        class="flex items-center"
+        :items="commentMenuItems"
+        @copyLink="copyLink"
+        @handleEdit="() => handleEdit(comment)"
+        @clickReport="handleReport"
+        @clickFeedback="
+          () => {
+            // This event is emitted when the user clicks give feedback in the comment menu.
+            // Passing the comment in at the template instead of the setup
+            // function or methods is better because it allows us to specify that
+            // we want a nested comment to be the target. If we did it in methods
+            // or setup, feedback would end up attached to the parent
+            // instead of the child.
+            handleFeedback({
+              commentData: comment,
+              parentCommentId: '',
+            });
+          }
+        "
+        @clickUndoFeedback="
+          () => {
+            // See comment on clickFeedback. The same principle applies.
+            handleUndoFeedback({ commentData: comment, parentCommentId: '' });
+          }
+        "
+        @handleViewFeedback="() => handleViewFeedback(comment.id)"
+        @handleDelete="
+          () => {
+            showDeleteCommentModal = true;
+          }
+        "
+        @clickEditFeedback="
+          () => {
+            // See comment on clickFeedback. The same principle applies.
+            handleEditFeedback({
+              commentData: comment,
+            });
+          }
+        "
+      >
+        <EllipsisHorizontal
+          class="h-5 w-5 cursor-pointer hover:text-black dark:text-gray-300 dark:hover:text-white"
+        />
+      </MenuButton>
     </div>
 
     <div class="ml-12 border-l-2 border-gray-200 dark:border-gray-500">
