3e368f0bb6c256600e61e111a223eee4f32d0267
Catherine Luse <gennitdev@gmail.com>
2024-08-17 20:56:02 -0700

    Stop console errors

--- src/components/comments/FeedbackSection.vue
+++ src/components/comments/FeedbackSection.vue
@@ -144,87 +144,84 @@ export default defineComponent({
 });
 </script>
 <template>
-  <h2 class="mt-4 text-wrap text-center text-xl font-bold dark:text-gray-200">
-    Feedback Comments ({{ feedbackCommentsAggregate }})
-  </h2>
-  <InfoBanner
-    v-if="feedbackCommentsAggregate > 0"
-    :text="'Feedback should focus on the writing, not the writer. If the feedback is rude or non-actionable, please report it.'"
-  />
-  <div
-    v-if="feedbackCommentsAggregate === 0"
-    class="text-center text-gray-500 dark:text-gray-300"
-  >
-    No feedback yet.
-  </div>
-  <PermalinkedFeedbackComment
-    v-if="showPermalinkedFeedback && feedbackId"
-    :key="feedbackId"
-    class="mt-2"
-    :comment-id="feedbackId"
-  >
-    <template #comment="{ commentData }">
+  <div>
+    <h2 class="mt-4 text-wrap text-center text-xl font-bold dark:text-gray-200">
+      Feedback Comments ({{ feedbackCommentsAggregate }})
+    </h2>
+    <InfoBanner
+      v-if="feedbackCommentsAggregate > 0"
+      :text="'Feedback should focus on the writing, not the writer. If the feedback is rude or non-actionable, please report it.'"
+    />
+    <div
+      v-if="feedbackCommentsAggregate === 0"
+      class="text-center text-gray-500 dark:text-gray-300"
+    >
+      No feedback yet.
+    </div>
+    <PermalinkedFeedbackComment
+      v-if="showPermalinkedFeedback && feedbackId"
+      :key="feedbackId"
+      class="mt-2"
+      :comment-id="feedbackId"
+    >
+      <template #comment="{ commentData }">
+        <CommentOnFeedbackPage
+          :comment="commentData"
+          :is-highlighted="true"
+          @showCopiedLinkNotification="showCopiedLinkNotification = true"
+          @clickFeedback="handleClickGiveFeedback"
+          @clickUndoFeedback="handleClickUndoFeedback"
+          @clickEditFeedback="handleClickEditFeedback"
+        />
+      </template>
+    </PermalinkedFeedbackComment>
+    <div v-for="comment in feedbackComments" :key="comment.id">
       <CommentOnFeedbackPage
-        :comment="commentData"
-        :is-highlighted="true"
+        v-if="
+          !showPermalinkedFeedback ||
+          (showPermalinkedFeedback && comment.id !== feedbackId)
+        "
+        :comment="comment"
         @showCopiedLinkNotification="showCopiedLinkNotification = true"
         @clickFeedback="handleClickGiveFeedback"
         @clickUndoFeedback="handleClickUndoFeedback"
         @clickEditFeedback="handleClickEditFeedback"
       />
-    </template>
-  </PermalinkedFeedbackComment>
-  <div
-    v-for="comment in feedbackComments"
-    :key="comment.id"
-  >
-    <CommentOnFeedbackPage
-      v-if="
-        !showPermalinkedFeedback ||
-          (showPermalinkedFeedback && comment.id !== feedbackId)
-      "
-      :comment="comment"
-      @showCopiedLinkNotification="showCopiedLinkNotification = true"
-      @clickFeedback="handleClickGiveFeedback"
-      @clickUndoFeedback="handleClickUndoFeedback"
-      @clickEditFeedback="handleClickEditFeedback"
+    </div>
+    <LoadMore
+      v-if="!loading && !reachedEndOfResults"
+      :reached-end-of-results="reachedEndOfResults"
+      @loadMore="() => loadMore()"
+    />
+    <div v-if="loading">Loading...</div>
+    <Notification
+      :show="showCopiedLinkNotification"
+      :title="'Copied to clipboard!'"
+      @closeNotification="showCopiedLinkNotification = false"
+    />
+    <GenericFeedbackFormModal
+      :open="showFeedbackFormModal"
+      :loading="addFeedbackCommentToCommentLoading"
+      :error="addFeedbackCommentToCommentError"
+      @close="$emit('closeFeedbackFormModal')"
+      @updateFeedback="updateFeedback"
+      @primaryButtonClick="handleSubmitFeedback"
+    />
+    <ConfirmUndoCommentFeedbackModal
+      v-if="showConfirmUndoFeedbackModal && commentToRemoveFeedbackFrom"
+      :key="loggedInUserModName"
+      :open="showConfirmUndoFeedbackModal"
+      :comment-id="commentToRemoveFeedbackFrom.id"
+      :comment-to-remove-feedback-from="commentToRemoveFeedbackFrom"
+      :mod-name="loggedInUserModName"
+      @close="showConfirmUndoFeedbackModal = false"
+    />
+    <EditCommentFeedbackModal
+      v-if="showEditCommentFeedbackModal"
+      :open="showEditCommentFeedbackModal"
+      :comment-id="commentToGiveFeedbackOn?.id || ''"
+      :mod-name="loggedInUserModName"
+      @close="showEditCommentFeedbackModal = false"
     />
   </div>
-  <LoadMore
-    v-if="!loading && !reachedEndOfResults"
-    :reached-end-of-results="reachedEndOfResults"
-    @loadMore="() => loadMore()"
-  />
-  <div v-if="loading">
-    Loading...
-  </div>
-  <Notification
-    :show="showCopiedLinkNotification"
-    :title="'Copied to clipboard!'"
-    @closeNotification="showCopiedLinkNotification = false"
-  />
-  <GenericFeedbackFormModal
-    :open="showFeedbackFormModal"
-    :loading="addFeedbackCommentToCommentLoading"
-    :error="addFeedbackCommentToCommentError"
-    @close="$emit('closeFeedbackFormModal')"
-    @updateFeedback="updateFeedback"
-    @primaryButtonClick="handleSubmitFeedback"
-  />
-  <ConfirmUndoCommentFeedbackModal
-    v-if="showConfirmUndoFeedbackModal && commentToRemoveFeedbackFrom"
-    :key="loggedInUserModName"
-    :open="showConfirmUndoFeedbackModal"
-    :comment-id="commentToRemoveFeedbackFrom.id"
-    :comment-to-remove-feedback-from="commentToRemoveFeedbackFrom"
-    :mod-name="loggedInUserModName"
-    @close="showConfirmUndoFeedbackModal = false"
-  />
-  <EditCommentFeedbackModal
-    v-if="showEditCommentFeedbackModal"
-    :open="showEditCommentFeedbackModal"
-    :comment-id="commentToGiveFeedbackOn?.id || ''"
-    :mod-name="loggedInUserModName"
-    @close="showEditCommentFeedbackModal = false"
-  />
 </template>
